69b8154c75f3d8216b747e2ce26763e9cd32e8a4
Kaguorawambui <bettywambui18@gmail.com>
2022-12-16 10:53:13 -0800

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,13 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b)
+if (a > b && a > c)
 {
-if (a > c)
 largest = a;
 }
-else
+else if (a > b && c > a)
 {
 largest = c;
-}
 }
 else if (b > c)
 {
@@ -31,5 +29,5 @@ else
 largest = c;
 }
 
-return (largest);
+return (largesr);
 }
