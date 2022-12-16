cc1cb5a20112e41e80f34262b5a8376baf0681a5
Kaguorawambui <bettywambui18@gmail.com>
2022-12-16 03:05:17 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_isupper - checks for uppercase character
+ * @c: variable text
+ * Return: Always 0.
+ */
+int_isupper(int c)
+{
+
+if (c >= 'A' && C <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
